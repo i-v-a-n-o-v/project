f47213124604cee1aa784984e55bf363c367eb1a
Dmitriy <ivanov.dv@nspk.ru>
2021-11-20 18:58:38 +0300

    Зависимости и env

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/", summary="Get a hello world json")
+def hello(
+    name: str = "World",
+):
+    """
+    Hello world view
+    1. processes `request`
+    1. returns greeting
+    """
+    return {"Hello": name}
+
+
+
